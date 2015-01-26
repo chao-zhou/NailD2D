047dc7e12d0f798a788500aeb3bba5cb6f5d0ced
chao-zhou <eebread@gmail.com>
2015-01-26 16:04:11 +0800

    add service interface

---
+++ app/src/main/java/com/naild2d/android/service/Service.java
@@ -0,0 +1,12 @@
+package com.naild2d.android.service;
+
+/**
+ * Created by chao_zhou on 15/1/26.
+ */
+public interface Service {
+
+    public void Config();
+
+    public void Config(ServiceConfig config);
+
+}
