75e02a22631b6051b9aacb8c8c2ca8673d690a69
chao-zhou <eebread@gmail.com>
2015-06-10 22:41:45 +0800

    添加Service Thumbnail

--- app/src/main/java/com/naild2d/android/component/ImageTextView2.java
+++ app/src/main/java/com/naild2d/android/component/ImageTextView2.java
@@ -1,8 +1,6 @@
 package com.naild2d.android.component;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -15,15 +13,15 @@ import com.naild2d.android.R;
 /**
  * Created by ebread on 2015/4/12.
  */
-public class ImageTextView extends LinearLayout {
+public class ImageTextView2 extends LinearLayout {
     private TextView textView;
     private ImageView imageView;
 
-    public ImageTextView(Context context) {
+    public ImageTextView2(Context context) {
         super(context);
     }
 
-    public ImageTextView(Context context, AttributeSet attrs) {
+    public ImageTextView2(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -32,20 +30,21 @@ public class ImageTextView extends LinearLayout {
         textView = (TextView) findViewById(R.id.component_image_text_view_text);
         imageView = (ImageView) findViewById(R.id.component_image_text_view_image);
 
-        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.ImageTextView);
+        /*TypedArray array = context.obtainStyledAttributes(attrs,com.android.internal.R.styleable.TextView);
+        String text = array.getString(com.android.internal.R.styleable.TextView_text);
+        float textSize = array.getFloat(com.android.internal.R.styleable.TextView_textSize,22);
+        int textColor = array.getColor(com.android.internal.R.styleable.TextView_textColor,Color.BLACK);
 
-        String text = array.getString(R.styleable.ImageTextView_text);
-        float textSize = array.getFloat(R.styleable.ImageTextView_textSize, 22);
-        int textColor = array.getColor(R.styleable.ImageTextView_textColor, Color.BLACK);
-        Drawable image = array.getDrawable(R.styleable.ImageTextView_image);
+        array = context.obtainStyledAttributes(attrs,com.android.internal.R.styleable.ImageView);
+        Drawable image = array.getDrawable(com.android.internal.R.styleable.ImageView_src);
+        //Need to get layout
 
         textView.setText(text);
         textView.setTextSize(textSize);
         textView.setTextColor(textColor);
 
         imageView.setImageDrawable(image);
-
-        array.recycle();
+*/
 
     }
 
---
+++ app/src/main/java/com/naild2d/android/component/ServiceThumbnailView.java
@@ -0,0 +1,51 @@
+package com.naild2d.android.component;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.naild2d.android.R;
+
+/**
+ * Created by ebread on 2015/5/26.
+ */
+public class ServiceThumbnailView extends RelativeLayout {
+
+    public ImageTextView nameView;
+    public TextView priceView;
+    public ImageView thumbnailView;
+
+    public ServiceThumbnailView(Context context) {
+        super(context);
+    }
+
+    public ServiceThumbnailView(Context context, AttributeSet attrs) throws Exception {
+        super(context, attrs);
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.component_service_thumbnail, this);
+
+        nameView = (ImageTextView) findViewById(R.id.component_service_thumbnail_view_name);
+        priceView = (TextView) findViewById(R.id.component_service_thumbnail_view_price);
+        thumbnailView = (ImageView) findViewById(R.id.component_service_thumbnail_view_thumbnail);
+
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.ServiceThumbnailView);
+
+        String name = array.getString(R.styleable.ServiceThumbnailView_name);
+        String price = array.getString(R.styleable.ServiceThumbnailView_integer_price);
+        Drawable thumbnail = array.getDrawable(R.styleable.ServiceThumbnailView_thumbnail);
+
+
+        nameView.setText(name);
+        priceView.setText(price);
+        thumbnailView.setImageDrawable(thumbnail);
+
+        array.recycle();
+    }
+
+}
