6f9336fe757cf1c6bec8d8d24a0d0becc241d69d
chao-zhou <eebread@gmail.com>
2015-07-15 21:12:02 +0800

    添加EvaluationActivity

---
+++ app/src/main/java/com/naild2d/android/activity/EvaluationActivity.java
@@ -0,0 +1,15 @@
+package com.naild2d.android.activity;
+
+import android.os.Bundle;
+
+import com.naild2d.android.R;
+
+public class EvaluationActivity extends NailD2DActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluation);
+    }
+
+}
